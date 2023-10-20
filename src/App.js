ffe4a35c4f6eefca954d3fd19ce76f50ffe05dc5
codefinger10 <codefinger10@gmail.com>
2023-10-20 17:21:22 +0900

    feat : 프로젝트 파일 정리 및 최적화
    유종의 미

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+
+function App() {
+  return (
+       <div class="wrap">
+        <Header/>
+          <div class="main">하단</div>
+          <Footer/>
+
+       </div>
+  );
+  }
+
+
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,4 @@
+export default function Header() {
+  //   js 코드 자리
+  return <header class="header">상단</header>;
+}
