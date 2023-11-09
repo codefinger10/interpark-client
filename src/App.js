31bd5ae9ddde67b7314e4f7790df439ee8c596a7
codefinger10 <codefinger10@gmail.com>
2023-11-09 11:40:26 +0900

    feat : 기존 Source Merge

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import Footer from "./components/Footer";
 import Header from "./components/Header";
-
-
+import Recommend from "./components/Recommend";
+import Visual from "./components/Visual";
 
 function App() {
+  // 안녕하세요. js 자리입니다.
+  /* 여러 줄 설명이에요 */
   return (
-       <div class="wrap">
-        <Header/>
-          <div class="main">하단</div>
-          <Footer/>
-
-       </div>
+    <div className="wrap">
+      {/* <!-- 상단 영역 --> */}
+      <Header />
+      {/* <!-- 메인 영역 --> */}
+      <div className="main">
+        <Visual />
+        <Recommend />
+      </div>
+      {/* <!-- 하단 영역 --> */}
+      <footer className="footer"></footer>
+    </div>
   );
-  }
-
-
+}
 
 export default App;
